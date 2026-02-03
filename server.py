e678da460a071c38b8b67c1c4e59f90b3faf76e5
martingpa <57588515+martingpa@users.noreply.github.com>
2026-02-03 16:12:53 +0100

    Update server.py
    sync

--- server.py
+++ server.py
@@ -1,17 +1,23 @@
 import socket
 
 s = socket.socket()
+print("Socket succesfully created")
 
 port = 12345
 
-s.bind("", port)
+s.bind(('', port))
+print(f"socket binded to {port}")
+
 s.listen(5)
+print("socket is listening")
 
 while True:
     
-    c, addr = s.accep()
-    c.send("Thank you for connecting from", addr)
-    c.send("Thank you for connecting".encode())
+    c, addr = s.accept()
+    print("Got connection from", addr)
+    
+    c.send('Thank you for connecting'.encode())
+    print(c.recv(1024).decode())
     
     c.close()
-    break
+    
